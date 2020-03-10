9141dfd8ba7107a45095420909c20def4320fb5a
yhoang <office.yhoang@gmail.com>
2020-03-10 12:14:57 +0100

    checked on zenbook

--- PRI-ana/Start_PRI-ana.R
+++ PRI-ana/Start_PRI-ana.R
@@ -16,7 +16,7 @@ fcs$version="v0.39c"
 ### "lenovoz570"  : laptop Lenovo Z570
 ### "asus-zenbook": ASUS zenbook
 ### ----------------------------------------------------------------------
-work.station="delta"
+work.station="asus-zenbook"
 # work.station="lenovoz570"
 # work.station="rev"
 # work.station="office"
@@ -24,13 +24,13 @@ work.station="delta"
 # work.station="Praktika"
 
 if (work.station == "asus-zenbook") {
-  PRIana.path = file.path("/","scratch","drfz_PRI","functions")
+  PRIana.path = file.path("","scratch","drfz_PRI","PRI-ana","functions")
 } else if (work.station == "drfz") {
   PRIana.path = file.path("Y:","AG_Baumgrass","AG-PRI","PRIanalyzer","functions")
 } else if (work.station == "delta") {
   PRIana.path = file.path("","scratch","drfz","PRI","PRI-ana","functions")
 } else if (work.station == "lenovoz570") {
-  PRIana.path = file.path("/","scratch","drfz_PRI","functions")
+  PRIana.path = file.path("","scratch","drfz_PRI","functions")
 } else if (work.station == "Ria") {
   PRIana.path = file.path("C:","Users","ag-baumgras","Documents","R","functions")
 }
@@ -38,7 +38,7 @@ if (work.station == "asus-zenbook") {
 ### Load functions -------------------------------------------------------
 # setwd(PRIana.path)
 ### Source several R scripts
-for (nm in list.files(path = PRIana.path, pattern = "\\.[RrSsQq]$")) {
+for (nm in list.files(path = PRIana.path, pattern = "\\.[Rr]$")) {
   source(file.path(PRIana.path, nm))
 }
 
