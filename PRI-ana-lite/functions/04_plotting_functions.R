47257ed353125ff8161b3875e7d83a53b0837cef
Flohrke <Felix.lohrke@gmx.net>
2020-09-17 15:30:59 +0200

    added script for plotting and log

---
+++ PRI-ana-lite/functions/04_plotting_functions.R
@@ -0,0 +1,11 @@
+#!/usr/bin/R
+# Author: Yen Hoang and Felix Lohrke
+# DRFZ 2020
+
+# ---------- # Functions for plotting # ---------- #
+
+# plotting a Histogram from marker data with threshold
+
+Main$plotHistogram <- function(markerdata, threshold) {
+   
+}
